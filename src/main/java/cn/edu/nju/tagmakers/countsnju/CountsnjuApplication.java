51abb03db4ed5c35f43ff3be4e857ae94c19ca0e
161250170 <161250170@smail.nju.edu.cn>
2018-04-06 13:56:28 +0800

    First commit

---
+++ src/main/java/cn/edu/nju/tagmakers/countsnju/CountsnjuApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.nju.tagmakers.countsnju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountsnjuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountsnjuApplication.class, args);
+	}
+}
