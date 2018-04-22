cf78f13d558897946305ea702d7b54fbf40fb1c4
161250170 <161250170@smail.nju.edu.cn>
2018-04-22 18:45:35 +0800

    judger接口修改

--- src/main/java/cn/edu/nju/tagmakers/countsnju/algorithm/ResultJudger.java
+++ src/main/java/cn/edu/nju/tagmakers/countsnju/algorithm/ResultJudger.java
@@ -4,6 +4,8 @@ import cn.edu.nju.tagmakers.countsnju.entity.pic.Description;
 import cn.edu.nju.tagmakers.countsnju.entity.pic.Edge;
 import cn.edu.nju.tagmakers.countsnju.entity.pic.Rect;
 
+import java.util.List;
+
 /**
  * Description:
  * 用于判断标注结果正误
@@ -17,11 +19,12 @@ public class ResultJudger {
         throw new UnsupportedOperationException();
     }
 
-    public static boolean judgeRect(Rect a, Rect b) {
+
+    public static boolean judgeRect(List<Rect> a, List<Rect> b) {
         throw new UnsupportedOperationException();
     }
 
-    public static boolean judgeEdge(Edge a, Edge b) {
+    public static boolean judgeEdge(List<Edge> a, List<Edge> b) {
         throw new UnsupportedOperationException();
     }
 }
