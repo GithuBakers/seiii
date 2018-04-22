8dea6fe2f5446a8fdd09fcc5358aabb19d8819dd
161250170 <161250170@smail.nju.edu.cn>
2018-04-22 23:25:22 +0800

    并发程序！

---
+++ src/main/java/cn/edu/nju/tagmakers/countsnju/algorithm/Cluster.java
@@ -0,0 +1,123 @@
+package cn.edu.nju.tagmakers.countsnju.algorithm;
+
+import cn.edu.nju.tagmakers.countsnju.entity.pic.Rect;
+
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.Collectors;
+
+/**
+ * Description:
+ * 将结果聚集为
+ *
+ * @author xxz
+ * Created on 04/22/2018
+ */
+public class Cluster {
+    public List<Rect> clusterRect(List<Rect> src) {
+        //临域半径 最小数
+        int R = 10, MIN = 5;
+
+        List<AlgoRect> algoRects = src.parallelStream().map(AlgoRect::new).collect(Collectors.toList());
+        AvxVector[] vectors = new AvxVector[src.size()];
+        algoRects.parallelStream()
+                .map(AlgoRect::getAvxVector)
+                .collect(Collectors.toList())
+                .toArray(vectors);
+
+        Map<AvxVector, Integer> indexMap = new HashMap<>();
+        for (int i = 0; i < src.size(); i++) {
+            indexMap.put(vectors[i], i);
+        }
+
+        //计算距离
+        double distance[][] = new double[src.size()][src.size()];
+
+//        System.out.println(distance+" out here");
+        distance(vectors, distance);
+        for (int i = 0; i < src.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                distance[i][j] = distance[j][i];
+            }
+        }
+
+
+        //计算核心对象
+        Set<AvxVector> cores = new HashSet<>();
+        for (int i = 0; i < src.size(); i++) {
+            int cnt = 0;
+            for (int k = 0; k < src.size(); k++) {
+                if (distance[i][k] < R) {
+                    cnt++;
+                }
+            }
+            if (cnt >= MIN) {
+                cores.add(vectors[i]);
+            }
+        }
+
+        List<Set<AvxVector>> clusters = new LinkedList<>();
+        //全局必须只有一份avxVector的引用
+        Set<AvxVector> unvisited = Arrays.stream(vectors).collect(Collectors.toSet());
+
+        while (!cores.isEmpty()) {
+            Set<AvxVector> oldUnvisited = new HashSet<>(unvisited);
+            ArrayList<AvxVector> q = new ArrayList<>();
+            AvxVector next = cores.iterator().next();
+            q.add(next);
+            cores.remove(next);
+            unvisited.remove(next);
+            while (!q.isEmpty()) {
+                AvxVector cur = q.get(0);
+                q.remove(cur);
+                LinkedList<AvxVector> neighbour = new LinkedList<>();
+                int i = indexMap.get(cur);
+                for (int j = 0; j < src.size(); j++) {
+                    if (distance[i][j] <= R && unvisited.contains(vectors[j])) {
+                        neighbour.add(vectors[j]);
+                    }
+                }
+
+                if (neighbour.size() >= MIN) {
+                    q.addAll(neighbour.stream()
+                            .filter(unvisited::contains)
+                            .collect(Collectors.toSet()));
+                    unvisited.removeAll(neighbour);
+                }
+
+            }
+            Set<AvxVector> newCluster = oldUnvisited.parallelStream()
+                    .filter(vector -> !unvisited.contains(vector))
+                    .collect(Collectors.toSet());
+            clusters.add(newCluster);
+            cores.removeAll(newCluster);
+        }
+
+        return clusters.parallelStream()
+                .filter(avxVectors -> avxVectors.size() != 0)
+                //每个簇取平均
+                .map(avxVectors -> avxVectors.parallelStream()
+                        .reduce(AvxVector::add)
+                        .orElse(avxVectors.iterator().next())
+                        .scale(1.0 / avxVectors.size()))
+
+                .map(AvxVector::getData)
+                .map(AlgoRect::new)
+                .map(AlgoRect::getRect)
+                .collect(Collectors.toList());
+    }
+
+    private void distance(AvxVector[] vectors, double[][] distance) {
+        DistanceCalculator distanceCalculator = new DistanceCalculator(distance, 0, vectors.length, vectors);
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        forkJoinPool.submit(distanceCalculator);
+        try {
+            distanceCalculator.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/cn/edu/nju/tagmakers/countsnju/algorithm/DistanceCalculator.java
@@ -0,0 +1,56 @@
+package cn.edu.nju.tagmakers.countsnju.algorithm;
+
+import java.util.concurrent.RecursiveTask;
+
+/**
+ * Description:
+ *
+ * @author xxz
+ * Created on 04/22/2018
+ */
+public class DistanceCalculator extends RecursiveTask<Integer> {
+    private double[][] distance;
+    private int beginRow;
+    private int taskNumber;
+    private AvxVector[] data;
+    private int THRESHOLD = 100000;
+
+    public DistanceCalculator(double[][] distance, int beginRow, int taskNumber, AvxVector[] data) {
+        this.distance = distance;
+        this.beginRow = beginRow;
+        this.data = data;
+        this.taskNumber = taskNumber;
+    }
+
+
+    /**
+     * The main computation performed by this task.
+     *
+     * @return the result of the computation
+     */
+    @Override
+    protected Integer compute() {
+//        System.out.println("from " + beginRow + " " + (beginRow + taskNumber));
+        if (taskNumber < THRESHOLD) {
+            for (int i = beginRow; i < beginRow + taskNumber; i++) {
+//                System.out.println("line "+i);
+                for (int j = i; j < data.length; j++) {
+                    distance[i][j] = data[i].distance(data[j]);
+//                    System.out.println("distance," + i + "," + j + "," + distance[i][j]);
+                }
+            }
+//            System.out.println(distance + " in here");
+//            System.out.println("here");
+        } else {
+            //分成两份
+            DistanceCalculator part1 = new DistanceCalculator(distance, beginRow, taskNumber >> 1, data);
+            DistanceCalculator part2 = new DistanceCalculator(distance, beginRow + (taskNumber >> 1) - 1, (taskNumber >> 1) + 2, data);
+            part1.fork();
+            part2.fork();
+            part1.join();
+            part2.join();
+        }
+        //不需要返回值
+        return null;
+    }
+}
