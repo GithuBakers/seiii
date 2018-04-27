3d0bf1a0540c7cd8013fc761b2ad42bb8426b1bf
161250151 <161250151@smail.nju.edu.cn>
2018-04-27 21:59:07 +0800

    修复关系DAO中读写文件的bug

--- src/main/java/cn/edu/nju/tagmakers/countsnju/data/dao/WorkerAndCriterionDAO.java
+++ src/main/java/cn/edu/nju/tagmakers/countsnju/data/dao/WorkerAndCriterionDAO.java
@@ -3,10 +3,16 @@ package cn.edu.nju.tagmakers.countsnju.data.dao;
 import cn.edu.nju.tagmakers.countsnju.entity.Criterion.Criterion;
 import cn.edu.nju.tagmakers.countsnju.entity.Criterion.WorkerAndCriterion;
 import cn.edu.nju.tagmakers.countsnju.exception.InvalidInputException;
+import cn.edu.nju.tagmakers.countsnju.filter.WorkerAndCriterionFilter;
 import org.springframework.stereotype.Component;
+import util.FileCreator;
+import util.Log;
+import util.LogPriority;
 
+import java.io.*;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Description:
@@ -21,11 +27,13 @@ import java.util.Set;
  */
 @Component
 public class WorkerAndCriterionDAO {
-    //todo
     private Set<WorkerAndCriterion> set;
+    private String filePath;
 
     public WorkerAndCriterionDAO() {
         set = new HashSet<>();
+        filePath = "data" + File.separator + "WorkerAndCriterion.txt";
+        read();
     }
 
     /**
@@ -50,21 +58,64 @@ public class WorkerAndCriterionDAO {
     /**
      * 检查过用户没有做过这个标准集之后添加这个结果
      *
-     * @param workerID
-     * @param criterion
      */
     public void add(String workerID, Criterion criterion) {
         if (criterion == null) throw new InvalidInputException("添加的标准集为空");
         WorkerAndCriterion temp = new WorkerAndCriterion(workerID, criterion);
         set.add(temp);
+        writeObject(set, filePath);
     }
 
+    /**
+     * 更新关系对象
+     */
     public void update(WorkerAndCriterion workerAndCriterion) {
         for (WorkerAndCriterion temp : set) {
             if (workerAndCriterion.getWorkerID().equals(temp.getWorkerID()) && workerAndCriterion.getCriterionID().equals(temp.getCriterionID())) {
                 set.remove(temp);
                 set.add(workerAndCriterion);
             }
+        }
+        writeObject(set, filePath);
+    }
+
+    /**
+     * 读对象
+     */
+    private Set<WorkerAndCriterion> readObj(String path) {
+        FileCreator.createFile(path);
+        Set<WorkerAndCriterion> objSet = null;
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File(path)))) {
+            objSet = (Set<WorkerAndCriterion>) ois.readObject();
+        } catch (EOFException e) {
+            objSet = new HashSet<>();
+        } catch (IOException | ClassNotFoundException e) {
+            Log.log("读对象异常", LogPriority.ERROR);
+            e.printStackTrace();
+        }
+        return objSet;
+    }
+
+    /**
+     * 读文件
+     */
+    private void read() {
+        Set<WorkerAndCriterion> objSet = readObj(filePath);
+        for (WorkerAndCriterion temp : objSet) {
+            set.add(temp);
+        }
+    }
+
+    /**
+     * 写文件
+     */
+    private void writeObject(Set<WorkerAndCriterion> objSet, String path) {
+        FileCreator.createFile(path);
+        try (ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(new File(path)))) {
+            os.writeObject(objSet);
+        } catch (IOException e) {
+            Log.log("写对象异常", LogPriority.ERROR);
+            e.printStackTrace();
         }
     }
 }
